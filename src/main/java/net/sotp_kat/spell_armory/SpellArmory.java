7cd6394211a8d539a1f1308023cb4130f6f9339f
sotp_kat <152227372+sotp-kat@users.noreply.github.com>
2024-08-22 12:03:53 +0200

    Corrected mistake in README

--- src/main/java/net/sotp_kat/spell_armory/SpellArmory.java
+++ src/main/java/net/sotp_kat/spell_armory/SpellArmory.java
@@ -9,14 +9,10 @@ import net.sotp_kat.spell_armory.registries.BrewingRegistry;
 import net.sotp_kat.spell_armory.registries.CreativeTabRegistry;
 import net.sotp_kat.spell_armory.registries.ItemRegistry;
 import net.sotp_kat.spell_armory.registries.LootModifierRegistry;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 @Mod(SpellArmory.MODID)
 public class SpellArmory {
-
-    public static final Logger LOGGER = LogManager.getLogger(SpellArmory.MODID); //LogUtils.getLogger();
 
     public static final String MODID = "spell_armory";
 
